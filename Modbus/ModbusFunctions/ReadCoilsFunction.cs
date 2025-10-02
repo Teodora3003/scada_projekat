0f24e3f4d3215a2e08833a5891c948e061b8a983
Teodora Klaric <teodoraklaric@gmail.com>
2025-10-02 22:55:53 +0200

    drugi komit uradjena prva tacka

--- Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -25,14 +25,61 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters paranCon = this.CommandParameters as ModbusReadCommandParameters;
+
+            byte[] request = new byte[12];
+
+            Buffer.BlockCopy(
+                    (Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.TransactionId)), 0,(Array)request,0,2);                 
+
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.ProtocolId)), 0, (Array)request, 2, 2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.Length)), 0, (Array)request, 4, 2);
+            request[6] = paranCon.UnitId;
+            request[7] = paranCon.FunctionCode;
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.StartAddress)), 0, (Array)request, 8, 2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.Quantity)), 0, (Array)request, 10, 2);
+
+            return request; 
+                            //throw new NotImplementedException();
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            
+            ModbusReadCommandParameters paramCon = this.CommandParameters as ModbusReadCommandParameters;
+
+            Dictionary<Tuple<PointType, ushort>, ushort> d = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int q = response[8];
+
+            
+            for (int i = 0; i < q; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    
+                    if ((j + i * 8) >= paramCon.Quantity)
+                    {
+                        break;
+                    }
+
+                    ushort v = (ushort)(response[9 + i] & 0x01);    
+
+                    response[9 + i] /= 1;   
+                    d.Add(                                  
+                        new Tuple<PointType, ushort>(           
+                            PointType.DIGITAL_OUTPUT,
+                            (ushort)(paramCon.StartAddress + (j + i * 8))
+                        ),
+                        v
+                    );
+                }
+            }
+
+            return d;
+            //throw new NotImplementedException();
         }
     }
 }
